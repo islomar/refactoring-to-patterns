9d207b05c8af83793b1ad420f884e6f5ceba026c
Isidro Lopez <islomar@gmail.com>
2016-03-06 22:32:59 +0100

    [session2] Extract constants CACHE_FOLER and REMOTE_FOLDER

--- session-2-procedural-to-objects/phpimageresize/Options.php
+++ session-2-procedural-to-objects/phpimageresize/Options.php
@@ -1,28 +1,25 @@
 <?php
 
-/**
- * Created by IntelliJ IDEA.
- * User: islomar
- * Date: 06/03/16
- * Time: 21:36
- */
-class Options {
+class Options
+{
+
+    const CACHE_FOLDER = './cache/';
+    const REMOTE_FOLDER = './cache/remote/';
 
     private $opts;
 
-    public function __construct($opts=array())
+    public function __construct($opts = array())
     {
-        $cacheFolder = './cache/';
-        $remoteFolder = $cacheFolder.'remote/';
 
         $defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false,
             'canvas-color' => 'transparent', 'output-filename' => false,
-            'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
+            'cacheFolder' => self::CACHE_FOLDER, 'remoteFolder' => self::REMOTE_FOLDER, 'quality' => 90, 'cache_http_minutes' => 20);
 
         $this->opts = array_merge($defaults, $opts);
     }
 
-    public function asHash() {
+    public function asHash()
+    {
         return $this->opts;
     }
 
